57eda4bbb7dc5b2a6a35ba05c14d90786c44fec6
Ewerton <ewerton13batista@gmail.com>
2022-11-28 20:26:11 -0300

    removed container

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,67 +11,64 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 420,
-      child: transactions.isEmpty
-          ? Column(
-              children: [
-                const SizedBox(
-                  height: 20,
+    return transactions.isEmpty
+        ? Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Nenhuma transação cadastrada',
+                style: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontSize: 25,
                 ),
-                const Text(
-                  'Nenhuma transação cadastrada',
-                  style: TextStyle(
-                    fontFamily: 'OpenSans',
-                    fontSize: 25,
-                  ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 200,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
-                )
-              ],
-            )
-          : ListView.builder(
-              itemCount: transactions.length,
-              itemBuilder: (ctx, index) {
-                final e = transactions[index];
-                return Card(
-                  elevation: 2,
-                  margin: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: FittedBox(
-                          child: Text('R\$${e.value}'),
-                        ),
+              )
+            ],
+          )
+        : ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (ctx, index) {
+              final e = transactions[index];
+              return Card(
+                elevation: 2,
+                margin: EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: FittedBox(
+                        child: Text('R\$${e.value}'),
                       ),
                     ),
-                    title: Text(
-                      e.title,
-                      style: TextStyle(
-                        fontFamily: 'OpenSans',
-                      ),
-                    ),
-                    subtitle: Text(
-                      DateFormat('d MMM y').format(e.date),
-                    ),
-                    trailing: IconButton(
-                        onPressed: () => onRemove(e.id),
-                        icon: const Icon(Icons.delete_forever),
-                        color: Theme.of(context).errorColor),
                   ),
-                );
-              },
-            ),
-    );
+                  title: Text(
+                    e.title,
+                    style: TextStyle(
+                      fontFamily: 'OpenSans',
+                    ),
+                  ),
+                  subtitle: Text(
+                    DateFormat('d MMM y').format(e.date),
+                  ),
+                  trailing: IconButton(
+                      onPressed: () => onRemove(e.id),
+                      icon: const Icon(Icons.delete_forever),
+                      color: Theme.of(context).errorColor),
+                ),
+              );
+            },
+          );
   }
 }

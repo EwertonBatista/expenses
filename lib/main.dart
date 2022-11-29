c42d4178f8624e870fe91f006d98824444d3aa4e
Ewerton <ewerton13batista@gmail.com>
2022-11-28 21:31:02 -0300

    add orientation landscape and change from Switch to IconButton to show Chart or List

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = [];
+  bool _showChart = false;
 
   List<Transaction> get _recentTransactions {
     return _transactions
@@ -86,9 +87,23 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    bool isLandScape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+
     final appBar = AppBar(
       title: Text('Despesas Pessoais'),
       actions: <Widget>[
+        if (isLandScape)
+          Row(
+            children: [
+              IconButton(
+                icon: Icon(_showChart ? Icons.list : Icons.bar_chart_rounded),
+                onPressed: () => setState(() {
+                  _showChart = !_showChart;
+                }),
+              ),
+            ],
+          ),
         IconButton(
           icon: Icon(Icons.add),
           onPressed: () => _openTransactionFormModal(context),
@@ -105,15 +120,17 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              height: availableHeight * 0.3,
-              child: Chart(recentsTransactions: _recentTransactions),
-            ),
-            Container(
-              height: availableHeight * 0.7,
-              child: TransactionList(
-                  transactions: _transactions, onRemove: _deleteTransaction),
-            ),
+            if (_showChart || !isLandScape)
+              Container(
+                height: availableHeight * (isLandScape ? 0.8 : 0.3),
+                child: Chart(recentsTransactions: _recentTransactions),
+              ),
+            if (!_showChart || !isLandScape)
+              Container(
+                height: availableHeight * (isLandScape ? 1.1 : 0.7),
+                child: TransactionList(
+                    transactions: _transactions, onRemove: _deleteTransaction),
+              ),
           ],
         ),
       ),
